8a9d69991e5e62b313c72045f8ac416d765a8aee
azharghafoor39 <azharghafoor39@gmail.com>
2026-01-09 22:37:34 +0500

    Fix os.path.isdir typo in reset script

--- scripts/reset_db.py
+++ scripts/reset_db.py
@@ -86,7 +86,7 @@ def reset_database():
                 try:
                     if os.path.isfile(file_path) or os.path.islink(file_path):
                         os.unlink(file_path)
-                    elif os.path.is_dir(file_path):
+                    elif os.path.isdir(file_path):
                         shutil.rmtree(file_path)
                 except Exception as e:
                     print(f"Failed to delete {file_path}. Reason: {e}")
